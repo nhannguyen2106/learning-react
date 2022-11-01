3a6ba2b54ebb3856e85307adadd02d3050f00ad5
Nhan Nguyen <nhannguyen2195@gmail.com>
2022-11-01 17:07:04 +0700

    practice 2

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,24 +1,75 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./styles.css";
+import { useState } from "react";
 
 const Form = (props) => {
+  const [title, setTitle] = useState("");
+  const [creator, setCreator] = useState("");
+  const [description, setDescription] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleOnSave = (e) => {
+    e.preventDefault();
+    if (title.length == 0 || creator.length == 0 || description.length == 0) {
+      setError(true);
+      return;
+    }
+    props.onClick();
+  };
+
   return (
-    <form className="addTaskForm">
+    <form className="addTaskForm" onSubmit={handleOnSave}>
       <div className="row-item">
         <label for="title">Title</label>
-        <input type="text" placeholder="Place holder"></input>
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="Place holder"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          {error && title.length <= 0 ? (
+            <div className="error">Title is required</div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
       <div className="row-item">
         <label for="creator">Creator</label>
-        <input type="text" placeholder="Name of Creator"></input>
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="Name of Creator"
+            value={creator}
+            onChange={(e) => setCreator(e.target.value)}
+          ></input>
+          {error && creator.length <= 0 ? (
+            <div className="error">Creator is required</div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
       <div className="row-item">
         <label for="description">Description</label>
-        <input type="text" placeholder="Description Details"></input>
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="Description Details"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></input>
+          {error && description.length <= 0 ? (
+            <div className="error">Description is required</div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
       <div className="btn">
-        <button type="button" className="saveBtn" onClick={props.onClick}>
+        <button type="submit" className="saveBtn">
           Save
         </button>
       </div>
